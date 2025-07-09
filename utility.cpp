2acbe443ec7311b7db4ae88bb3ac9bb94f8713ba
Leopixelo <leonardgies01@gmail.com>
2025-07-09 20:23:30 +0200

    add support for start date offset

--- utility.cpp
+++ utility.cpp
@@ -2,5 +2,7 @@
 
 bool Utility::decimalMode = false;
 QString Utility::timeFormat = "MMM-dd HH:mm:ss.zzz";
+QDateTime Utility::startDate = QDateTime::fromString("2025-01-01T00:00:00.000000", Qt::ISODateWithMs);
+bool Utility::useStartDate = false;
 TimeStyle Utility::timeStyle = TS_MICROS;
 QString Utility::fullyQualifiedNameSeperator = "::";
